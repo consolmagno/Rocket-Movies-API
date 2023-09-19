2612111a9754a3946c0201d83c82686140feeac4
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-09-19 18:20:43 -0300

    create database user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,20 @@
-const { request } = require("express");
-
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite")
+
 class UserController{
-  create(request, response){
+  async create(request, response){
     const {name, email, password} = request.body;
 
-    if(!name){
-      throw new AppError("Nome é obrigatório")
-    }
-    response.status(201).json({name, email, password});
-  }
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
+    if(checkUserExists){
+      throw new AppError("Esse e-mail já está em uso.")
+    }
+    await database.run("INSERT INTO users (name, email, password) VALUES(?, ?, ?)", [name, email, password])
+    return response.status(201).json();
+  }
 }
 
 module.exports = UserController
